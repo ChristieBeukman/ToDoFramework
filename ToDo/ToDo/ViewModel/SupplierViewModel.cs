1553b4de163d4540710f8e59d2c52c4adfeb54f9
Christie Beukman <me.mcbeukman@gmail.com>
2017-08-11 00:14:41 +0200

    textbox style added

--- ToDo/ToDo/ViewModel/SupplierViewModel.cs
+++ ToDo/ToDo/ViewModel/SupplierViewModel.cs
@@ -3,20 +3,91 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using ToDo.Services;
+using ToDo.Model;
 
 
 namespace ToDo.ViewModel
 {
     public class SupplierViewModel : ViewModelBase
     {
+        /// <summary>
+        /// Private Public Properties
+        /// </summary>
         IDataAccessSupplier _ServiceProxy;
+        private ObservableCollection<Suppliier> _Suppliers;
+        private Suppliier _Supplier;
+        private Suppliier _SelectedSupplier;
 
+        /// <summary>
+        /// Public Properties
+        /// </summary>
+        public ObservableCollection<Suppliier> Suppliers
+        {
+            get
+            {
+                return _Suppliers;
+            }
+
+            set
+            {
+                _Suppliers = value;
+                RaisePropertyChanged("Suppliers");
+            }
+        }
+
+        public Suppliier Supplier
+        {
+            get
+            {
+                return _Supplier;
+            }
+
+            set
+            {
+                _Supplier = value;
+                RaisePropertyChanged("Supplier");
+            }
+        }
+
+        public Suppliier SelectedSupplier
+        {
+            get
+            {
+                return _SelectedSupplier;
+            }
+
+            set
+            {
+                _SelectedSupplier = value;
+                RaisePropertyChanged("SelectedSupplier");
+            }
+        }
+
+        /// <summary>
+        /// COnstructor Initialises all objects
+        /// </summary>
+        /// <param name="prxy"></param>
         public SupplierViewModel(IDataAccessSupplier prxy)
         {
             _ServiceProxy = prxy;
+            Suppliers = new ObservableCollection<Suppliier>();
+            Supplier = new Suppliier();
+            SelectedSupplier = new Suppliier();
+            GetSupplier();
         }
+
+        void GetSupplier()
+        {
+            Suppliers.Clear();
+            foreach (var item in _ServiceProxy.GetSuppliers())
+            {
+                Suppliers.Add(item);
+            }
+        }
+
     }
 }
