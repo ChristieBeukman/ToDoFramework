c06706bcc2ed529b33b3d02958c0c5ad75fa9db5
Christie Beukman <me.mcbeukman@gmail.com>
2017-08-12 21:12:16 +0200

    Delete added

--- ToDo/ToDo/ViewModel/SupplierViewModel.cs
+++ ToDo/ToDo/ViewModel/SupplierViewModel.cs
@@ -8,12 +8,16 @@ using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using ToDo.Services;
 using ToDo.Model;
-
+using System.Windows;
+using ToDo.View;
 
 namespace ToDo.ViewModel
 {
     public class SupplierViewModel : ViewModelBase
     {
+#region Properties
+
+#region Private
         /// <summary>
         /// Private Public Properties
         /// </summary>
@@ -21,7 +25,12 @@ namespace ToDo.ViewModel
         private ObservableCollection<Suppliier> _Suppliers;
         private Suppliier _Supplier;
         private Suppliier _SelectedSupplier;
+        private Suppliier _Sup;
 
+
+        #endregion Private
+
+ #region Public
         /// <summary>
         /// Public Properties
         /// </summary>
@@ -67,6 +76,32 @@ namespace ToDo.ViewModel
             }
         }
 
+        public Suppliier Sup
+        {
+            get
+            {
+                return _Sup;
+            }
+
+            set
+            {
+                _Sup = value;
+                RaisePropertyChanged("Sup");
+            }
+        }
+
+
+        #endregion Public
+
+#region Commands
+
+        public RelayCommand AddSupplierCommand { get; set; }
+        public RelayCommand OpenAddSupplierView { get; set; }
+        public RelayCommand DeleteSupplierCommand { get; set; }
+        #endregion
+        #endregion Properties
+
+        #region Constructor
         /// <summary>
         /// COnstructor Initialises all objects
         /// </summary>
@@ -78,8 +113,18 @@ namespace ToDo.ViewModel
             Supplier = new Suppliier();
             SelectedSupplier = new Suppliier();
             GetSupplier();
+            Sup = new Suppliier();
+            AddSupplierCommand = new RelayCommand(AddSupplier);
+            OpenAddSupplierView = new RelayCommand(OpenAddSupplierWindow);
+            DeleteSupplierCommand = new RelayCommand(DeleteSupplier);
         }
 
+        #endregion Constructor
+
+#region Methods
+        /// <summary>
+        /// Fill the Combobox with the database data
+        /// </summary>
         void GetSupplier()
         {
             Suppliers.Clear();
@@ -88,6 +133,49 @@ namespace ToDo.ViewModel
                 Suppliers.Add(item);
             }
         }
+
+        /// <summary>
+        /// Add new record to the database
+        /// </summary>
+        void AddSupplier()
+        {
+            Suppliers.Add(Sup);
+            _ServiceProxy.CreateSupplier(Sup);
+            RaisePropertyChanged("Sup");
+            MessageBox.Show(Sup + " has been succesfully added");
+        }
+
+        /// <summary>
+        /// Opens the Add Supplier Window
+        /// </summary>
+        void OpenAddSupplierWindow()
+        {
+            var win = new AddSupplierWindowView();
+            win.ShowDialog();
+        }
+
+        void DeleteSupplier()
+        {
+            var Result = MessageBox.Show("Delete " + SelectedSupplier.Name, "DELETE", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+            switch (Result)
+            {
+                case MessageBoxResult.Yes:
+                    _ServiceProxy.DeleteSupplier(SelectedSupplier);
+                    Suppliers.Remove(SelectedSupplier);
+                    MessageBox.Show("Deleted","",MessageBoxButton.OK,MessageBoxImage.Information);
+                    GetSupplier();
+                    RaisePropertyChanged("SelectedSupplier");
+                    RaisePropertyChanged("Suppliers");
+                    break;
+                case MessageBoxResult.No:
+
+                    break;
+                default:
+                    break;
+            }
+           
+        }
+#endregion Constructor
 
     }
 }
